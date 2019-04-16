d13f59b230ce2bf712fe097bf23aa2fe6ca9d24b
Brian Dombrowski <bdombro@gmail.com>
2019-04-16 18:41:07 -0400

    Initial commit, broken wiht error

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+import { Server } from 'warthog';
+
+export function getServer(AppOptions = {}) {
+  return new Server(
+    {
+      introspection: true,
+      warthogImportPath: 'warthog',
+      ...AppOptions
+    },
+    // Make sure TypeORM does not auto-update the DB schema so that we know our CLI commands
+    // are making the changes
+    { synchronize: false }
+  );
+}
